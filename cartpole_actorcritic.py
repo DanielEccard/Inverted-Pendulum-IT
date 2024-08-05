843fb2cab181b5f3e8e4b6e97406087f657c0a72
Gabriel Matz <85305743+gdsmatz@users.noreply.github.com>
2024-08-05 18:10:17 -0300

    Update cartpole_actorcritic.py

--- cartpole_actorcritic.py
+++ cartpole_actorcritic.py
@@ -1,206 +1,275 @@
-import collections
-import gym
-import numpy as np
-import statistics
-import tensorflow as tf
-import tqdm
 
-from matplotlib import pyplot as plt
-from typing import Any, List, Sequence, Tuple
+"""
+CartPole
+
+Agente- Observa o ambiente e escolhe uma ação, sendo a recompensa +1
+pra cada intervalo de tempo que permaneceu de pé. Se passar de 2.4m ou 24 graus
+termina. Os inputs são 4: posição, velocidade linear, velocidade angular e ângulo.
+São 2 outputs, andar para a direita ou esquerda. A rede vai prever qual terá melhor
+recompensa.
+
+"""
+
+import gym 
+import math
+import random 
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class CustomCartPoleEnv(gym.Wrapper):
+    def __init__(self, env, pole_length=0.5, cart_mass=1.0, pole_mass=0.1):
+        super(CustomCartPoleEnv, self).__init__(env)
+        self.env = env
+        self.env.unwrapped.length = pole_length  # Modifica o comprimento do pêndulo
+        self.env.unwrapped.masscart = cart_mass  # Modifica a massa do carro
+        self.env.unwrapped.masspole = pole_mass  # Modifica a massa do pêndulo
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        return self.env.step(action)
+
+# Parâmetros customizados
+pole_length = 0.39  # comprimento do pêndulo
+cart_mass = 0.850   # massa do carro
+pole_mass = 0.085    # massa do pêndulo
+
+env = gym.make("CartPole-v1", render_mode=None)
+custom_env = CustomCartPoleEnv(env, pole_length=pole_length, cart_mass=cart_mass, pole_mass=pole_mass)
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if GPU is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+"""
+ Replay Memory 
+ Replay memory é usado para guardar estados, ações e rewards coletados
+ pelo agente durante cada iteração em tuples, aumentando a eficiência
+ que aprende de experiências passadas, dando mais estabilidade.
+    Transition
+    state, action -> next_state, reward
+"""
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
 
-# Environment
-env = gym.make("CartPole-v1")
+class ReplayMemory(object):
 
-# Seed 
-seed = 42
-env.seed(seed)
-tf.random.set_seed(seed)
-np.random.seed(seed)
+    def __init__(self, capacity):
+        # Quando chega na capcidade máxima os mais antigos são eliminados
+        self.memory = deque([], maxlen=capacity)
 
-# Limita o epsilon
-eps = np.finfo(np.float32).eps.item()
+    def push(self, *args):
+        # Salva transições e adiciona novas experiências
+        self.memory.append(Transition(*args))
 
-class ActorCritic(tf.keras.Model):
+    def sample(self, batch_size):
+        # Amostras random ajudam a descorrelatar no tempo?
+        return random.sample(self.memory, batch_size)
 
-  def __init__(
-      self, 
-      num_actions: int, # No nosso caso 2
-      num_hidden_units: int):
-    """Initialize."""
-    super().__init__()
+    def __len__(self):
+        return len(self.memory)
+ 
+"""
+    DQN
+    Queremos achar a política que maximiza o reward, montando assim
+    um Q* que indica o reward futuro dado um certo estado e uma ação.
+    Aproximamos Q* para Q, que a rede neural irá achar, segundo Bellman
+    e depois aplica Huber Loss.
+    
+"""
 
-    self.common = tf.keras.layers.Dense(num_hidden_units, activation="relu")
-    self.actor = tf.keras.layers.Dense(num_actions)
-    self.critic = tf.keras.layers.Dense(1)
+class DQN(nn.Module):
 
-  def call(self, inputs: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
-    x = self.common(inputs)
-    return self.actor(x), self.critic(x)
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__() # inicialização
+        self.layer1 = nn.Linear(n_observations, 128) # input+1 hidden layer
+        self.layer2 = nn.Linear(128, 128) # +1 hidden layer
+        self.layer3 = nn.Linear(128, n_actions) # output, valores de Q para cada ação
+    # forward pass com ativação ReLu 
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
 
-num_actions = env.action_space.n  # 2
-num_hidden_units = 128
+"""
+    Target x online network
+    Online- Estima os Qs, com updates frequentes usando gradient descent, escolhendo a ação
+    Target- Mesma arquitetura da online, mas os updates são mais lentos. Parametros da online vão pra ela,
+    indicando estimativas melhores do Q
+"""
+BATCH_SIZE = 128 # indica samples no replay
+GAMMA = 0.99 # fator de desconto no Q
+EPS_START = 0.9 # fator de exploração inicial
+EPS_END = 0.05 # fator de exploração mínimo
+EPS_DECAY = 1000 # fator de decaimento da exploração
+TAU = 0.005 # taxa de update do target network
+LR = 1e-4 # learning rate do Adam
 
-model = ActorCritic(num_actions, num_hidden_units)
+# Número de ações
+n_actions = env.action_space.n
+# Observações
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions).to(device) # policy network
+target_net = DQN(n_observations, n_actions).to(device) # target network
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True) # usa Adam amsgrad
+memory = ReplayMemory(10000) # buffer com 10000 experiências
 
 
-def env_step(action: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-
-  state, reward, done, _ = env.step(action) # tira do env as infos
-  return (state.astype(np.float32), 
-          np.array(reward, np.int32), 
-          np.array(done, np.int32))
+steps_done = 0
 
 
-def tf_env_step(action: tf.Tensor) -> List[tf.Tensor]:
-  return tf.numpy_function(env_step, [action], 
-                           [tf.float32, tf.int32, tf.int32]) # transforma env step em tensores
-def run_episode(
-    initial_state: tf.Tensor,  
-    model: tf.keras.Model, 
-    max_steps: int) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+def select_action(state): # estado input, ação output. Toma ação com mais reward com p= epsilon ou ação exploratória p=1-epsilon
+    global steps_done
+    sample = random.random() # explora ou aproveita
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY) #eps greedy
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # se eps < sample, ação com maior reward
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long) # se sample< eps ação aleatória
 
-  action_probs = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
-  values = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
-  rewards = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
 
-  initial_state_shape = initial_state.shape
-  state = initial_state
+episode_durations = []
 
-  for t in tf.range(max_steps):
-    state = tf.expand_dims(state, 0) # expande para bater com o que o tf quer
 
-    # Tira a ação a partit do modelo
-    action_logits_t, value = model(state)
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # +100 episódios vê a média
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
 
-    # proxima ação
-    action = tf.random.categorical(action_logits_t, 1)[0, 0]
-    action_probs_t = tf.nn.softmax(action_logits_t)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        if not show_result:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+        else:
+            display.display(plt.gcf())
 
-    # guarda valores pro crítico avaliar
-    values = values.write(t, tf.squeeze(value))
-    action_probs = action_probs.write(t, action_probs_t[0, action])
 
-    # Aplica a ação no env do Gym
-    state, reward, done = tf_env_step(action)
-    state.set_shape(initial_state_shape)
-    rewards = rewards.write(t, reward)
+# Training loop
+# ^^^^^^^^^^^^^
 
-    if tf.cast(done, tf.bool):
-      break
+def optimize_model():
+    if len(memory) < BATCH_SIZE: # checa se tem memória antiga
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Ele separa os componentes de cada sample(state, actions...) para facilitar cálculos
+    # list(zip(*[('a', 1), ('b', 2), ('c', 3), ('d', 4)]))
+    # [('a', 'b', 'c', 'd'), (1, 2, 3, 4)]
+    batch = Transition(*zip(*transitions))
 
-  action_probs = action_probs.stack()
-  values = values.stack()
-  rewards = rewards.stack()
+    # Cria mask de estados não finais
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    # Forma tensores com cada batch
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
 
-  return action_probs, values, rewards
+    # Acha os Q values para as ações no estado atual usando as informações do batch na policy net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
 
-def get_expected_return(
-    rewards: tf.Tensor, 
-    gamma: float, 
-    standardize: bool = True) -> tf.Tensor:
-  """Compute expected returns per timestep."""
+    # Q values na target net
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+    # Q esperados segundo bellman
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
-  n = tf.shape(rewards)[0]
-  returns = tf.TensorArray(dtype=tf.float32, size=n)
-  rewards = tf.cast(rewards[::-1], dtype=tf.float32)
-  discounted_sum = tf.constant(0.0)
-  discounted_sum_shape = discounted_sum.shape
-  for i in tf.range(n):
-    reward = rewards[i]
-    discounted_sum = reward + gamma * discounted_sum
-    discounted_sum.set_shape(discounted_sum_shape)
-    returns = returns.write(i, discounted_sum)
-  returns = returns.stack()[::-1]
+    # Huber loss entre Q esperado e Q calculado
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
 
-  if standardize:
-    returns = ((returns - tf.math.reduce_mean(returns)) / 
-               (tf.math.reduce_std(returns) + eps))
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    # Clip dos gradientes para evitar que fiquem muito grandes?
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step() #muda o modelo
 
-  return returns
-# Huber Loss
-huber_loss = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.SUM)
 
-def compute_loss(
-    action_probs: tf.Tensor,  
-    values: tf.Tensor,  
-    returns: tf.Tensor) -> tf.Tensor:
-  """Computes the combined actor-critic loss."""
+if torch.cuda.is_available():
+    num_episodes = 600
+else:
+    num_episodes = 250
 
-  advantage = returns - values
+for i_episode in range(num_episodes):
+    # Reinicia o ambiente
+    state, info = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    for t in count():
+        action = select_action(state) # seleciona ação
+        observation, reward, terminated, truncated, _ = env.step(action.item()) # executa ação
+        reward = torch.tensor([reward], device=device)
+        done = terminated or truncated
 
-  action_log_probs = tf.math.log(action_probs)
-  actor_loss = -tf.math.reduce_sum(action_log_probs * advantage)
+        if terminated:
+            next_state = None
+        else:
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
 
-  critic_loss = huber_loss(values, returns)
+        # Guarda transição
+        memory.push(state, action, next_state, reward)
 
-  return actor_loss + critic_loss
+        # Próximo estado
+        state = next_state
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+        # Muda os parâmetros da Q network
+        optimize_model()
 
-@tf.function
-def train_step(
-    initial_state: tf.Tensor, 
-    model: tf.keras.Model, 
-    optimizer: tf.keras.optimizers.Optimizer, 
-    gamma: float, 
-    max_steps_per_episode: int) -> tf.Tensor:
-  """Runs a model training step."""
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        target_net.load_state_dict(target_net_state_dict)
 
-  with tf.GradientTape() as tape:
-
-    # 1 rodada
-    action_probs, values, rewards = run_episode(
-        initial_state, model, max_steps_per_episode) 
-
-    # retorno esperado
-    returns = get_expected_return(rewards, gamma)
-
-    action_probs, values, returns = [
-        tf.expand_dims(x, 1) for x in [action_probs, values, returns]] 
-
-    # loss pro backpropagation
-    loss = compute_loss(action_probs, values, returns)
-
-  # Gradientes
-  grads = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(grads, model.trainable_variables))
-  episode_reward = tf.math.reduce_sum(rewards)
-
-  return episode_reward
-
-min_episodes_criterion = 100
-max_episodes = 10000 # limite superior (esse código é rápido)
-max_steps_per_episode = 1000
-
-# "Cartpole-v1 is considered solved if average reward is >= 195 over 100"
-# Escolhi 300
-reward_threshold = 300
-running_reward = 0
-
-# Gamma para mudar o aprendizado
-gamma = 0.99
-
-episodes_reward: collections.deque = collections.deque(maxlen=min_episodes_criterion)
-
-with tqdm.trange(max_episodes) as t:
-  for i in t:
-    initial_state = tf.constant(env.reset(), dtype=tf.float32)
-    episode_reward = int(train_step(
-        initial_state, model, optimizer, gamma, max_steps_per_episode))
-
-    episodes_reward.append(episode_reward)
-    running_reward = statistics.mean(episodes_reward)
-
-    t.set_description(f'Episode {i}')
-    t.set_postfix(
-        episode_reward=episode_reward, running_reward=running_reward)
-
-    # Show average episode reward every 10 episodes
-    if i % 10 == 0:
-      pass # print(f'Episode {i}: average reward: {avg_reward}')
-
-    if running_reward > reward_threshold and i >= min_episodes_criterion:  
-        break
-
-print(f'\nSolved at episode {i}: average reward: {running_reward:.2f}!')
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
+torch.save(policy_net.state_dict(), 'policy_net.pth')
+torch.save(target_net.state_dict(), 'target_net.pth')
+print('Complete')
+plot_durations(show_result=True)
+plt.ioff()
+plt.show()
